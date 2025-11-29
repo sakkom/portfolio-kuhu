d063bf422947dc190aa4380a75c00ba0cac3e13e
sak h <sakh@saks-MacBook-Pro.local>
2025-11-29 14:00:56 +0900

    add glsl-school week2

---
+++ app/glsl-school/week2/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { week2Shader } from "./shader";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    const scene = new THREE.Scene();
+    // scene.background = new THREE.Color(0xfffff0);
+    scene.background = new THREE.Color(0x000000);
+    const cam = new THREE.PerspectiveCamera(
+      45,
+      canvas.width / canvas.height,
+      0.1,
+      100,
+    );
+    cam.position.set(0, 0, 5.0);
+    const controls = new OrbitControls(cam, canvas);
+    controls.rotateSpeed = 0.2;
+
+    const loader = new THREE.TextureLoader();
+    const materials: (THREE.ShaderMaterial | null)[] = new Array(2).fill(null);
+    for (let i = 0; i < 2; i++) {
+      loader.load("/checker-board.jpg", (tex0) => {
+        const aspect = tex0.image.width / tex0.image.height;
+        materials[i] = new THREE.ShaderMaterial({
+          uniforms: THREE.UniformsUtils.clone(week2Shader.uniforms),
+          vertexShader: week2Shader.vertexShader,
+          fragmentShader: week2Shader.fragmentShader,
+        });
+        materials[i]!.uniforms.uResolution.value.set(
+          window.innerWidth,
+          window.innerHeight,
+        );
+        materials[i]!.uniforms.uLayer.value = i;
+        materials[i]!.uniforms.uTexture0.value = tex0;
+        materials[i]!.side = THREE.DoubleSide;
+        const geometry = new THREE.PlaneGeometry(2 * aspect, 2, 100, 100);
+
+        const mesh = new THREE.Mesh(geometry, materials[i]!);
+        scene.add(mesh);
+      });
+    }
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      materials.forEach((m) => {
+        if (m) {
+          m.uniforms.uTime.value += 0.0167;
+        }
+      });
+      controls.update();
+      renderer.render(scene, cam);
+    };
+    animate();
+  }, []);
+  return <canvas ref={canvasRef} />;
+}
---
+++ app/glsl-school/week2/shader.ts
@@ -0,0 +1,64 @@
+import * as THREE from "three" ;
+export const week2Shader = {
+uniforms : {
+uResolution : { value : new THREE . Vector2(0.0, 0.0)},
+uTime : { value : 0.0 },
+uLayer : { value : 0.0 },
+uTexture0 : { value : null },
+},
+vertexShader : `
+precision mediump float;
+uniform sampler2D uTexture0;
+uniform float uTime;
+uniform float uLayer;
+varying vec2 vUv;
+
+//https://help.autodesk.com/view/MAYAUL/2015/JPN/?guid=Shading_Nodes_Luminance
+float lumi(vec3 color) {
+  return dot(color, vec3(0.3, 0.59, 0.11));
+}
+
+mat3 rotateX(float angle) {
+  float c = cos(angle), s = sin(angle);
+  return mat3(
+    1.0, 0.0, 0.0,
+    0.0, c, -s,
+    0.0, s, c
+  );
+}
+
+void main() {
+  vUv = uv;
+  vec4 bgTex = texture2D(uTexture0, uv);
+  float l = lumi(bgTex.rgb);
+  vec3 newPosition = position;
+  if (uLayer == 0.0) {
+    newPosition.z = l < 0.5 ? -1.0 : 1.0;
+  } else {
+    newPosition.z = l < 0.5 ? 1.0 : -1.0;
+  }
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+}
+`,
+fragmentShader : `
+precision mediump float;
+uniform vec2 uResolution;
+uniform float uTime;
+uniform sampler2D uTexture0;
+varying vec2 vUv;
+
+float lumi(vec3 color) {
+  return dot(color, vec3(0.3, 0.59, 0.11));
+}
+
+void main() {
+  vec2 uv = vUv;
+  for (int i = 0; i < 5; i++) {
+    vec3 col = texture2D(uTexture0, uv).rgb;
+    float l = lumi(col);
+    uv.x += (l - 0.5) * cos(uTime * 0.5) * 0.2;
+    uv.y += (l - 0.5) * sin(uTime * 0.5) * 0.2;
+  }
+  gl_FragColor = texture2D(uTexture0, uv);
+}
+` }
